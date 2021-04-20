88dd769ed39760eac1111beae77dc73b08a72206
rahibarot <rahibarot1997@gmail.com>
2021-04-20 18:28:40 -0400

    Create nonPubclasstester.java

---
+++ NonPublicClass/nonPubclasstester.java
@@ -0,0 +1,10 @@
+package com.training.employee;
+
+public class nonPubclasstester {
+		//this is the way you can access the non public class 
+		//make a class and const in the same package and make a new method 
+		public nonPubclasstester(){
+			nonPublicClass Mpub= new nonPublicClass();
+		}
+		
+}
